fd21987d956afc619d3554b214da6bf31a11f91e
aayush4vedi <4vedi.aayush@gmail.com>
2021-12-03 03:12:26 +0530

    added whatsapp

--- js/main.js
+++ js/main.js
@@ -264,6 +264,7 @@ $(".openmodale").click(function (e) {
   e.preventDefault();
   $(".modale").addClass("opened");
 });
+
 $(".closemodale").click(function (e) {
   e.preventDefault();
   $(".modale").removeClass("opened");
